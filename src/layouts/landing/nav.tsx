e89c748baea488c886d6d2ff2e4781ce1ec300b2
romeo <romeomeshack34@gmail.com>
2025-07-18 14:08:05 +0100

    Nav

--- src/layouts/landing/nav.tsx
+++ src/layouts/landing/nav.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { ListIcon, ArrowElbowDownRightIcon } from "@phosphor-icons/react";
 import Button from "../../shared-components/button";
@@ -10,6 +11,7 @@ import Button from "../../shared-components/button";
  * @returns ReactElement
  */
 export default function Nav() {
+  const [showNav, setShowNav] = useState(false);
   const location = useLocation();
 
   const pathname = location.pathname.toLowerCase();
@@ -20,8 +22,10 @@ export default function Nav() {
     { name: "Contact us", link: "/contact" },
   ];
 
+  const mobileNavDropdown = () => {};
+
   return (
-    <nav className="w-full h-auto flex items-center justify-between gap-2 py-[0.5rem] px-4 md:px-[7.5rem]">
+    <nav className="w-full h-auto flex items-center justify-between gap-2 py-[0.5rem] px-4 lg:px-[7.5rem] relative">
       <div className="flex items-center gap-6">
         <img
           src="/logo/logo.png"
@@ -62,7 +66,36 @@ export default function Nav() {
           className="hidden md:block"
           onClick={() => console.log("go to login")}
         />
-        <ListIcon className="text-primary-500 block md:hidden" size={24} />
+
+        {/* Mobile screen nav menu trigger burger icon */}
+        <div onClick={() => setShowNav(!showNav)}>
+          <ListIcon className="text-primary-500 block md:hidden" size={24} />
+        </div>
+      </div>
+
+      {/* Mobile screen nav menu */}
+      <div
+        className={`w-full ${
+          showNav ? "h-fit visible" : "h-[-3px] invisible"
+        } absolute top-[5rem] left-0 flex flex-col gap-4 bg-white py-[0.75rem] px-4  shadow-md transition-all md:!hidden`}
+      >
+        <Button
+          type="button"
+          text="Login"
+          variant="outline"
+          className={`w-full ${showNav ? "visible" : "invisible"}`}
+          onClick={() => console.log("Login")}
+        />
+
+        <div
+          className={`flex flex-col gap-4 font-light ${
+            showNav ? "visible" : "invisible"
+          }`}
+        >
+          <Link to="/home">Home</Link>
+          <Link to="/about">About us</Link>
+          <Link to="/contact">Contact us</Link>
+        </div>
       </div>
     </nav>
   );
