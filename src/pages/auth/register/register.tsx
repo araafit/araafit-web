b38f57ec4148a1aad0f962c3db617be09feaba69
romeo <romeomeshack34@gmail.com>
2025-07-24 22:56:29 +0100

    registration

--- src/pages/auth/register/register.tsx
+++ src/pages/auth/register/register.tsx
@@ -9,8 +9,13 @@ import {
 import { useForm, FormProvider } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { formSteps } from "./form-steps-data";
+import Modal from "../../../shared-components/modal";
+import { useSwitch } from "../../../hooks/switch";
+import checkmark from "../checkmark.png";
 import Spinner from "../../../shared-components/spinner";
-/* --------------------------------------------------- */
+import Button from "../../../shared-components/button";
+// import { useNavigate } from "react-router-dom";
+/* ------------------------------------------------------------------------- */
 
 export type FormValues = {
   firstName: string;
@@ -51,8 +56,11 @@ const StepContent = ({ step }: { step: number }) => {
  * @returns ReactElement
  */
 export default function Register() {
-  const [isLoading, _] = useState(false);
   const [currentStep, setCurrentStep] = useState(0);
+  const { toggleSwitch, switchValue: isOpen } = useSwitch(false);
+  const [isLoading, setLoading] = useState(false);
+
+  // const navigate = useNavigate();
 
   const methods = useForm<FormValues>({
     mode: "onTouched",
@@ -78,9 +86,12 @@ export default function Register() {
   };
 
   const onSubmit = async (data: FormValues) => {
-    alert(`Verification code submitted: ${data.verificationCode.join("")}`);
-    // await new Promise((res) => setTimeout(res, 1500));
-    // setLoading(!isLoading);
+    console.log("form data", data);
+    setLoading(!isLoading);
+
+    await new Promise((res) => setTimeout(res, 1500));
+    setLoading(false);
+    toggleSwitch();
   };
 
   return (
@@ -91,31 +102,56 @@ export default function Register() {
         googleAutBtnText={formSteps[currentFormStep].googleAutBtnText}
         googleAuthTrigger={formSteps[currentFormStep].googleAuthTrigger}
       >
-        <form className="w-full flex flex-col gap-6">
-          <StepContent step={currentStep} />
+        <>
+          <form className="w-full flex flex-col gap-6">
+            <StepContent step={currentStep} />
 
-          <button
-            type="submit"
-            onClick={onNext}
-            className={`w-full text-white px-5 py-3 rounded-md ${
-              !methods.formState.isValid ? "bg-neutral-50" : "bg-primary-500"
-            } capitalize`}
-            disabled={methods.formState.isValid ? false : true}
-          >
-            {currentStep === formSteps.length - 1 ? (
-              <div>
-                <span>Submit</span>
-                {isLoading && <Spinner size="sm" speed="fast" />}
-              </div>
-            ) : (
-              "Continue"
-            )}
-          </button>
+            <button
+              type="submit"
+              onClick={onNext}
+              className={`w-full text-white px-5 py-3 rounded-md ${
+                !methods.formState.isValid ? "bg-neutral-50" : "bg-primary-500"
+              } capitalize`}
+              disabled={methods.formState.isValid ? false : true}
+            >
+              {currentStep === formSteps.length - 1 ? (
+                <div className="flex items-center justify-center">
+                  <span>Submit</span>
+                  {isLoading && (
+                    <Spinner size="sm" speed="fast" className="ml-1" />
+                  )}
+                </div>
+              ) : (
+                "Continue"
+              )}
+            </button>
 
-          <Link to="/auth/login" className="text-center">
-            Existing User? <span className="text-primary-500">Login</span>
-          </Link>
-        </form>
+            <Link to="/auth/login" className="text-center">
+              Existing User? <span className="text-primary-500">Login</span>
+            </Link>
+          </form>
+
+          <Modal isOpen={isOpen} containerClassName="w-[25rem]">
+            <div className="w-full max-w-[] flex flex-col items-center justify-center gap-3">
+              <img src={checkmark} alt="" className="w-[5.25rem] h-auto" />
+
+              <strong className="font-lora font-medium text-[2rem]">
+                Welcome Aboard!
+              </strong>
+
+              <p className="text-neutral-700 leading-araafit text-center font-light">
+                Your account has been successfully created.
+              </p>
+
+              <Button
+                text="Go to dashboard"
+                variant="solid"
+                className="w-full"
+                onClick={() => console.log("Should navigate to dashboard")}
+              />
+            </div>
+          </Modal>
+        </>
       </AuthLayout>
     </FormProvider>
   );
