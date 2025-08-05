81fde71e21af371a7e1983dc490c640c19c97617
romeo <romeomeshack34@gmail.com>
2025-08-05 23:45:28 +0100

    Added detail

--- src/pages/dashboard/import-entry.tsx
+++ src/pages/dashboard/import-entry.tsx
@@ -1,5 +1,6 @@
 export { DashboardHomePage } from "./home";
 export { DashboardShopPage } from "./shop";
-export { DashboardOrdersPage } from "./orders";
+export { DashboardOrdersPage } from "./orders/orders";
+export { DashboardOrderDetailPage } from "./orders/order-detail";
 export { DashboardCartPage } from "./cart";
 export { DashboardProfilePage } from "./profile";
