b1acee6e17b86139564dbd0350e585ab13f5b220
romeo <romeomeshack34@gmail.com>
2025-08-09 20:08:42 +0100

    Testimonials

--- src/pages/landing/home/testimonials.tsx
+++ src/pages/landing/home/testimonials.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import "./slider-style.css";
 
 /* ---------------------------------------------------- */
 
@@ -13,25 +12,28 @@ export default function Testimonials({
 }) {
   const [activeSlide, setActiveSlide] = useState(0);
 
+  const slideToShow = 3;
   const sliderSettings = {
-    autoPlay: true,
+    autoPlay: false,
     arrows: false,
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: slideToShow,
+    slidesToScroll: 3,
     pauseOnDotsHover: true,
-    afterChange: (idx: number) => setActiveSlide(idx),
-    customPaging: (idx: number) => (
-      <div className={`custom-dot ${idx === activeSlide ? "active" : ""}`} />
-    ),
-    dotsClass: "slick-dots custom-dots",
+    afterChange: (current: number) => setActiveSlide(() => current / slideToShow),
+    customPaging: (idx: number) => ( <div
+        className={`custom-dot size-[13px] rounded-full bg-[#F9F9F9] border-2 border-neutral-100 px-[5px] transition-all delay-[0.3s] ease-linear ${
+          idx === activeSlide ? "!bg-primary-950 !border-primary-950" : ""
+        }`}
+      />),
+    dotsClass: "slick-dots !flex items-center justify-center pt-[20px]",
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 1,
           slidesToScroll: 3,
           infinite: true,
           dots: true,
@@ -55,10 +57,15 @@ export default function Testimonials({
     ],
   };
 
+  console.log(activeSlide)
+
   return (
     <Slider className="w-full h-[236px]" {...sliderSettings}>
       {data.map((item, idx) => (
-        <div key={idx} className="w-full max-w-[357px] flex flex-col justify-center gap-4 p-4">
+        <div
+          key={idx}
+          className="w-full max-w-[357px] flex flex-col justify-center gap-4 p-4"
+        >
           <p className="text-neutral-950 font-light mb-4">{item.comment}</p>
 
           <div className="text-primary-500">
