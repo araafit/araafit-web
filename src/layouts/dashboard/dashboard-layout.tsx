74f94b155a41df57e1ae0c4df071995afe04c09d
romeo <romeomeshack34@gmail.com>
2025-08-09 16:29:02 +0100

    Logout modal

--- src/layouts/dashboard/dashboard-layout.tsx
+++ src/layouts/dashboard/dashboard-layout.tsx
@@ -8,6 +8,9 @@ import {
 } from "@phosphor-icons/react";
 import { Link } from "react-router-dom";
 import DashboardLoader from "./loader";
+import Modal from "../../shared-components/modal";
+import { useSwitch } from "../../hooks/switch";
+import Button from "../../shared-components/button";
 
 /* ------------------------------------------------------ */
 
@@ -29,6 +32,8 @@ export default function DashboardLayout({
 }: {
   children: React.ReactElement;
 }) {
+  const { toggleSwitch, switchValue } = useSwitch(false);
+
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -51,46 +56,51 @@ export default function DashboardLayout({
               />
             </Link>
 
-            {navMenu.map((item, idx) => {
-              if (item.name.toLowerCase() !== "profile") {
+            <div className="flex flex-col gap-4">
+              {navMenu.map((item, idx) => {
+                if (item.name.toLowerCase() !== "profile") {
+                  return (
+                    <Link
+                      to={item.link}
+                      key={idx}
+                      className="w-full flex flex-col gap-4 p-[0.5rem] text-neutral-900 hover:bg-primary-900 hover:text-white"
+                    >
+                      <div
+                        key={idx}
+                        className="w-full flex items-center text-base"
+                      >
+                        {createElement(item.icon ? item.icon : "a", {
+                          className: "mr-3",
+                        })}
+                        <span className="capitalize text-sm">{item.name}</span>
+                      </div>
+                    </Link>
+                  );
+                }
+
                 return (
                   <Link
                     to={item.link}
                     key={idx}
                     className="w-full flex flex-col gap-4 p-[0.5rem] text-neutral-900 hover:bg-primary-900 hover:text-white"
                   >
-                    <div
-                      key={idx}
-                      className="w-full flex items-center text-base"
-                    >
-                      {createElement(item.icon ? item.icon : "a", {
-                        className: "mr-3",
-                      })}
+                    <div className="w-full flex items-center text-base">
+                      <span className="uppercase mr-3 size-[20px] text-[10px] border border-primary-50 p-1 rounded-full flex items-center justify-center">
+                        en
+                      </span>
+
                       <span className="capitalize text-sm">{item.name}</span>
                     </div>
                   </Link>
                 );
-              }
-
-              return (
-                <Link
-                  to={item.link}
-                  key={idx}
-                  className="w-full flex flex-col gap-4 p-[0.5rem] text-neutral-900 hover:bg-primary-900 hover:text-white"
-                >
-                  <div className="w-full flex items-center text-base">
-                    <span className="uppercase mr-3 size-[20px] text-[10px] border border-primary-50 p-1 rounded-full flex items-center justify-center">
-                      en
-                    </span>
-
-                    <span className="capitalize text-sm">{item.name}</span>
-                  </div>
-                </Link>
-              );
-            })}
+              })}
+            </div>
           </div>
 
-          <div className="text-[0.875rem] flex items-center gap-[0.75rem] text-neutral-900 border-t-2 border-neutral-100 p-[0.5rem] hover:bg-primary-900 hover:text-white cursor-pointer">
+          <div
+            className="text-[0.875rem] flex items-center gap-[0.75rem] text-neutral-900 border-t-2 border-neutral-100 p-[0.5rem] hover:bg-primary-900 hover:text-white cursor-pointer"
+            onClick={toggleSwitch}
+          >
             <SignOutIcon />
             <span>Logout</span>
           </div>
@@ -98,6 +108,32 @@ export default function DashboardLayout({
       </div>
 
       <div className="grow">{loading ? <DashboardLoader /> : children}</div>
+
+      {/* Logout redirection modal */}
+      <Modal
+        onClose={toggleSwitch}
+        isOpen={switchValue}
+        containerClassName="w-full max-w-[25rem]"
+      >
+        <div className="flex flex-col items-center gap-3">
+          <h2 className="font-medium text-[2rem] text-neutral-950">Logout!</h2>
+          <p className="text-neutral-700 text-center">
+            Are you sure you want to logout of your account?
+          </p>
+
+          <Button
+            text="Cancel"
+            variant="clear"
+            className="w-full text-neutral-900 shadow-sm"
+          />
+
+          <Button
+            text="Logout"
+            variant="clear"
+            className="w-full bg-red-600 text-white"
+          />
+        </div>
+      </Modal>
     </section>
   );
 }
