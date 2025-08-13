eb05e2913ea559be2bcd89b38a37f1c987473b79
romeo <romeomeshack34@gmail.com>
2025-08-13 16:50:41 +0100

    Added checkout page

--- src/pages/dashboard/import-entry.tsx
+++ src/pages/dashboard/import-entry.tsx
@@ -2,5 +2,6 @@ export { DashboardHomePage } from "./home";
 export { DashboardShopPage } from "./shop";
 export { DashboardOrdersPage } from "./orders/orders";
 export { DashboardOrderDetailPage } from "./orders/order-detail";
-export { DashboardCartPage } from "./cart";
+export { DashboardCartPage } from "./cart/cart";
+export {DashboardCartCheckout} from "./cart/checkout"
 export { DashboardProfilePage } from "./profile";
