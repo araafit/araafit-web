8bfd15537907411dea572bf6eb555c91c49a4134
romeo <romeomeshack34@gmail.com>
2025-08-13 17:09:50 +0100

    Modified

---
+++ src/pages/dashboard/cart/checkout.tsx
@@ -0,0 +1,13 @@
+// import React from "react";
+
+/* ------------------------------------------ */
+
+/**
+ * Cart checkout
+ *
+ *
+ * @returns ReactElement
+ */
+export function DashboardCartCheckout() {
+  return <div>checkout</div>;
+}
