3f006bb59646c488b57bcdfb47f721e177245bc1
romeo <romeomeshack34@gmail.com>
2025-08-20 18:07:19 +0100

    Added card state

--- src/hooks/state-store.ts
+++ src/hooks/state-store.ts
@@ -7,8 +7,21 @@ import {
   type ReadyToWear,
   type RecommendedFabric,
 } from "../pages/dashboard/_data/_home";
-import { cartItems, type CartItem } from "../pages/dashboard/_data/_cart";
-/* --------------------------------------- */
+import {
+  cartItems,
+  paymentInfo,
+  deliveryInfo,
+  type CartItem,
+  type PaymentInfo,
+  type DeliveryInfo,
+} from "../pages/dashboard/_data/_cart";
+import {
+  measurementData,
+  type MeasurementData,
+} from "../pages/dashboard/_data/_profile";
+import { billingCards, type BillingCard } from "../pages/dashboard/_data/_card";
+
+/* --------------------------------------------------------------------------------------- */
 
 // ------------------- Auth ---------------------
 export const useAuthStore = create((set) => ({
@@ -31,6 +44,8 @@ export const useShopStore = create<ShopState>(() => ({
 // ------------------- Orders ---------------------
 interface OrderState {
   items: OrderItem[];
+  paymentInfo?: PaymentInfo[];
+  deliveryInfo?: DeliveryInfo[];
   addItem: (item: OrderItem) => void;
 }
 export const useOrdersStore = create<OrderState>((set) => ({
@@ -42,17 +57,65 @@ export const useOrdersStore = create<OrderState>((set) => ({
 // ------------------- Cart ---------------------
 export interface CartState {
   items: CartItem[];
+  paymentInfo?: PaymentInfo[];
+  deliveryInfo?: DeliveryInfo[];
   addItem: (item: CartItem) => void;
   removeItem: (orderId: string | number) => void;
 }
+
 export const useCartStore = create<CartState>((set) => ({
   items: cartItems,
-  addItem: (item: CartItem) =>
-    set((state: any) => ({ items: [...state.items, item] })),
-  removeItem: (orderId: string | number) =>
-    set((state) => {
+  addItem: (item: CartItem) => {
+    return set((state: any) => ({ items: [...state.items, item] }));
+  },
+  removeItem: (orderId: string | number) => {
+    return set((state) => {
       const newOrders = state.items.filter((item) => item.orderId !== orderId);
 
       return { items: newOrders };
+    });
+  },
+}));
+
+// --------- Profile ----------------
+
+export interface ProfileState {
+  myProfile: any[];
+  measurement?: MeasurementData;
+  card?: any[];
+  notification?: any[];
+}
+
+export const useProfileState = create<ProfileState>((set) => ({
+  myProfile: [],
+  measurement: measurementData,
+  card: [],
+  notification: [],
+}));
+
+// ----------- Card --------------------
+export interface CardState {
+  cards: BillingCard[];
+  selectedCard: number;
+  addCard?: (card: BillingCard) => void;
+  removeCard?: (CardId: number) => void;
+  selectCard?: (cardId: number) => void;
+}
+
+export const useCardState = create<CardState>((set) => ({
+  cards: billingCards,
+  selectedCard: 0,
+  addCard: (card: BillingCard) =>
+    set((state) => {
+      const cards = [...state.cards, card];
+
+      return { cards };
     }),
+  removeCard: (cardId: number) =>
+    set((state) => {
+      const cards = state.cards.filter((card) => card.id !== cardId);
+
+      return { cards };
+    }),
+  selectCard: (cardId: number) => set(() => ({ selectedCard: cardId })),
 }));
