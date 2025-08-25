5ad687a9a52d6f8689083d028d1acaf46b5b4977
romeo <romeomeshack34@gmail.com>
2025-08-25 16:15:12 +0100

    renamed to user-dashboard

--- src/pages/dashboard/import-entry.tsx
+++
@@ -1,8 +0,0 @@
-export { DashboardHomePage } from "./home";
-export { DashboardShopPage } from "./shop";
-export { DashboardOrdersPage } from "./orders/orders";
-export { DashboardOrderDetailPage } from "./orders/order-detail";
-export { DashboardCartPage } from "./cart/cart";
-export {DashboardCartCheckout} from "./cart/checkout/checkout"
-export { DashboardProfilePage } from "./profile/profile";
-export { DashboardEditMeasurementPage } from "./profile/measurement/edit-measured";
