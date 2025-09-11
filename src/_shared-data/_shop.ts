d0f2e211f1c7a161fe404a35311126f0d87e36b2
Romeo <romeo@Romeos-MacBook-Pro.local>
2025-09-11 10:56:15 +0100

    Shared data

---
+++ src/_shared-data/_shop.ts
@@ -0,0 +1,207 @@
+import {
+  rtw1,
+  rtw2,
+  rtw3,
+  recommendedRtw1,
+  recommendedRtw2,
+  recommendedRtw3,
+} from "../shared-images/image-entry";
+/* ------------------------------------------------- */
+interface Item {
+  name: string;
+  image: string;
+  cost: number | string;
+  category: string;
+}
+export interface Dresses extends Item {}
+export interface Fabrics extends Item {}
+export interface AllItems extends Item {}
+
+export const allItems: AllItems[] = [
+  {
+    name: "Araafit Orange & Cream Jumpsuit",
+    cost: 40_000,
+    image: rtw1,
+    category: "dress",
+  },
+  {
+    name: "Araafit All Blue Jumpsuit",
+    cost: 40_000,
+    image: rtw2,
+    category: "dress",
+  },
+  {
+    name: "Araafit Yellow & Black Jumpsuit",
+    cost: 70_000,
+    image: rtw3,
+    category: "dress",
+  },
+  {
+    name: "Araafit Red/Black Lace",
+    cost: 55_000,
+    image: recommendedRtw1,
+    category: "fabric",
+  },
+  {
+    name: "Araafit Golden Ankara",
+    cost: 25_000,
+    image: recommendedRtw3,
+    category: "fabric",
+  },
+  {
+    name: "Araafit Golden Ankara",
+    cost: 30_000,
+    image: recommendedRtw2,
+    category: "fabric",
+  },
+  {
+    name: "Araafit Orange & Cream Jumpsuit",
+    cost: 40_000,
+    image: rtw1,
+    category: "dress",
+  },
+  {
+    name: "Araafit All Blue Jumpsuit",
+    cost: 40_000,
+    image: rtw2,
+    category: "dress",
+  },
+  {
+    name: "Araafit Yellow & Black Jumpsuit",
+    cost: 70_000,
+    image: rtw3,
+    category: "dress",
+  },
+  {
+    name: "Araafit Red/Black Lace",
+    cost: 55_000,
+    image: recommendedRtw1,
+    category: "fabric",
+  },
+  {
+    name: "Araafit Golden Ankara",
+    cost: 25_000,
+    image: recommendedRtw3,
+    category: "fabric",
+  },
+  {
+    name: "Araafit Golden Ankara",
+    cost: 30_000,
+    image: recommendedRtw2,
+    category: "fabric",
+  },
+];
+export const fabrics: Fabrics[] = [
+  {
+    name: "Araafit Red/Black Lace",
+    cost: 55_000,
+    image: recommendedRtw1,
+    category: "fabric",
+  },
+  {
+    name: "Araafit Golden Ankara",
+    cost: 25_000,
+    image: recommendedRtw3,
+    category: "fabric",
+  },
+  {
+    name: "Araafit Golden Ankara",
+    cost: 30_000,
+    image: recommendedRtw2,
+    category: "fabric",
+  },
+  {
+    name: "Araafit Red/Black Lace",
+    cost: 55_000,
+    image: recommendedRtw1,
+    category: "fabric",
+  },
+  {
+    name: "Araafit Golden Ankara",
+    cost: 25_000,
+    image: recommendedRtw3,
+    category: "fabric",
+  },
+  {
+    name: "Araafit Golden Ankara",
+    cost: 30_000,
+    image: recommendedRtw2,
+    category: "fabric",
+  },
+  {
+    name: "Araafit Red/Black Lace",
+    cost: 55_000,
+    image: recommendedRtw1,
+    category: "fabric",
+  },
+  {
+    name: "Araafit Golden Ankara",
+    cost: 25_000,
+    image: recommendedRtw3,
+    category: "fabric",
+  },
+  {
+    name: "Araafit Golden Ankara",
+    cost: 30_000,
+    image: recommendedRtw2,
+    category: "fabric",
+  },
+];
+export const dresses: Dresses[] = [
+  {
+    name: "Araafit Orange & Cream Jumpsuit",
+    cost: 40_000,
+    image: rtw1,
+    category: "dress",
+  },
+  {
+    name: "Araafit All Blue Jumpsuit",
+    cost: 40_000,
+    image: rtw2,
+    category: "dress",
+  },
+  {
+    name: "Araafit Yellow & Black Jumpsuit",
+    cost: 70_000,
+    image: rtw3,
+    category: "dress",
+  },
+
+  {
+    name: "Araafit Yellow & Black Jumpsuit",
+    cost: 70_000,
+    image: rtw3,
+    category: "dress",
+  },
+  {
+    name: "Araafit Orange & Cream Jumpsuit",
+    cost: 40_000,
+    image: rtw1,
+    category: "dress",
+  },
+  {
+    name: "Araafit All Blue Jumpsuit",
+    cost: 40_000,
+    image: rtw2,
+    category: "dress",
+  },
+
+  {
+    name: "Araafit Orange & Cream Jumpsuit",
+    cost: 40_000,
+    image: rtw1,
+    category: "dress",
+  },
+  {
+    name: "Araafit All Blue Jumpsuit",
+    cost: 40_000,
+    image: rtw2,
+    category: "dress",
+  },
+  {
+    name: "Araafit Yellow & Black Jumpsuit",
+    cost: 70_000,
+    image: rtw3,
+    category: "dress",
+  },
+];
