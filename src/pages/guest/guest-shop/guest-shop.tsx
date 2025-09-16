bb25dd1165cb3bf6dd47af8e1914ac96d33c68d7
Romeo <romeo@Romeos-MacBook-Pro.local>
2025-09-16 20:34:17 +0100

    Removed top searchbar

--- src/pages/guest/guest-shop/guest-shop.tsx
+++ src/pages/guest/guest-shop/guest-shop.tsx
@@ -4,7 +4,6 @@ import AllItems from "../../../shared-components/shop/all-items";
 import DressItems from "../../../shared-components/shop/dress-items";
 import FabricItems from "../../../shared-components/shop/fabric-items";
 import { SearchProvider } from "../../user-dashboard/shop/context/search-context";
-import SearchInput from "../../user-dashboard/shop/components/search-input";
 import { XIcon } from "@phosphor-icons/react";
 import { useEffect } from "react";
 import Button from "../../../shared-components/button";
@@ -106,16 +105,6 @@ export function GuestShopPage() {
       <>
         <SearchProvider>
           <div className="w-full lg:w-[71.875rem] p-8 bg-white rounded-md overflow-y-scroll relative">
-            {/* Search Section */}
-            <div className="mb-6">
-              <div className="flex items-center justify-between mb-4">
-                <h2 className="text-lg font-semibold text-gray-900">
-                  Shop Products
-                </h2>
-                <SearchInput />
-              </div>
-            </div>
-
             {/* Shop Tabs */}
             <div className="size-full bg-white">
               <ShopTab
