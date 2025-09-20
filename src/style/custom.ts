98abc743f6f08255ce8a4d8e9430166383ce4234
Romeo <romeomeshack34@gmail.com>
2025-09-20 08:37:41 +0100

    custom styles

---
+++ src/style/custom.ts
@@ -0,0 +1,14 @@
+/* Alert */
+export const notificationStyles = {
+  alertSuccess: {
+    backgroundColor: "#F6FEF9",
+    color: "#15803c",
+    border: "1px solid #15803C",
+  },
+  alertError: {
+    backgroundColor: "#fca5a1",
+    color: "#b91c1c",
+    border: "1px solid #fca5a1",
+  },
+};
+/* End Alert */
