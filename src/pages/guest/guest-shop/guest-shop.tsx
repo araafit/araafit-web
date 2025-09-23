b1a59a58b8b83675fa6175a10e16434fbde432ff
Romeo <romeomeshack34@gmail.com>
2025-09-23 18:42:21 +0100

    Guest shop

--- src/pages/guest/guest-shop/guest-shop.tsx
+++ src/pages/guest/guest-shop/guest-shop.tsx
@@ -100,7 +100,7 @@ export function GuestShopPage() {
   const { switchValue: isOpen, toggleSwitch: toggleModal } = useSwitch(false);
 
   useEffect(() => {
-    setTimeout(() => toggleModal(), 3000);
+    setTimeout(() => toggleModal(), 10000);
   }, []);
 
   return (
