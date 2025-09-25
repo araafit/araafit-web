9233ebe1e9f9f01233c771f23841b153aa5ce27d
Romeo <romeomeshack34@gmail.com>
2025-09-25 17:36:02 +0100

    Guest shop

--- src/pages/guest/guest-shop/guest-shop.tsx
+++ src/pages/guest/guest-shop/guest-shop.tsx
@@ -17,7 +17,7 @@ export function GuestShopPage() {
     <GuestPageLayout>
       <>
         <SearchProvider>
-          <div className="w-full lg:w-[71.875rem] p-8 bg-white rounded-md overflow-y-scroll relative">
+          <div className="w-full h-full lg:w-[71.875rem] p-8 bg-white rounded-md overflow-y-scroll relative mb-10">
             {/* Shop Tabs */}
             <div className="size-full bg-white">
               <ShopTab
