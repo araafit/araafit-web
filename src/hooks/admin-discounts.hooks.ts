09c31cac0ecd138250e54d466f19e4fad92ec1f6
Romeo <romeomeshack34@gmail.com>
2025-10-22 13:09:59 +0100

    Modified

--- src/hooks/admin-discounts.hooks.ts
+++ src/hooks/admin-discounts.hooks.ts
@@ -138,19 +138,3 @@ export const useToggleDiscount = () => {
   });
 };
 
-export const useActiveDiscounts = () => {
-  return useQuery({
-    queryFn: () => adminDiscountsService.getActiveDiscounts(),
-    queryKey: discountsKeys.activeDiscount(),
-    staleTime: 2 * 60 * 1000,
-    retry: (failureCount, error: unknown) => {
-      const axiosError = error as { response?: { status?: number } };
-      if (
-        axiosError?.response?.status === 401 ||
-        axiosError?.response?.status === 403
-      )
-        return false;
-      return failureCount < 1;
-    },
-  });
-};
