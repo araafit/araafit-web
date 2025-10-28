c94bf10e1678f9f555bf5fd50f1a2da64c3c8c61
Romeo <romeomeshack34@gmail.com>
2025-10-28 09:15:27 +0100

    Gender selection component

---
+++ src/pages/get-measured/manual/pick-gender.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import Button from "../../../shared-components/button";
+import { maleImage, femaleImage } from "../image-export";
+import { useNavigate } from "react-router-dom";
+/* ------------------------------------------------------------ */
+
+export function PickGender() {
+  const [selectedGender, setSelectedGender] = useState<
+    "male" | "female" | null
+  >(null);
+  const navigate = useNavigate();
+
+  return (
+    <section className="min-h-screen bg-[#F5F5F5] px-0 py-0 md:py-2 md:px-16 overflow-y-scroll relative">
+      <div className="w-full min-h-[809px] bg-white border rounded-md p-4 md:p-14 space-y-20">
+        <div className="w-full flex items-center justify-center">
+          <div>
+            <h1 className="text-lg sm:text-[2rem] text-[#1C1C1C] font-semibold">
+              Select Gender
+            </h1>
+
+            <p className="text-neutral-500 text-[1rem] text-center">
+              I am a...
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full flex items-center justify-center gap-[3.375rem]">
+          <div
+            className={`h-auto w-full max-w-[25.25rem] bg-[#0C506E] rounded-3xl ${
+              selectedGender === "male"
+                ? "border-8 border-primary-950"
+                : "border-2 border-transparent hover:border-primary-800"
+            } cursor-pointer`}
+            onClick={() => setSelectedGender("male")}
+          >
+            <img src={maleImage} alt="" className="size-full object-contain" />
+          </div>
+
+          <div
+            className={`h-auto w-full max-w-[25.25rem] bg-[#9A6C50] rounded-3xl ${
+              selectedGender === "female"
+                ? "border-8 border-primary-950"
+                : "border-2 border-transparent hover:border-primary-800"
+            } cursor-pointer`}
+            onClick={() => setSelectedGender("female")}
+          >
+            <img
+              src={femaleImage}
+              alt=""
+              className="size-full object-contain"
+            />
+          </div>
+        </div>
+
+        <div className="w-full block sm:flex items-center justify-end gap-5 md:mr-40">
+          <Button
+            text="Back"
+            variant="outline"
+            className="w-full sm:w-[175px] text-neutral-950 border-neutral-100 mb-5 sm:mb-0"
+            onClick={() => navigate("/get-measured")}
+          />
+
+          <Button
+            text="Continue"
+            variant="solid"
+            disabled={selectedGender === null}
+            className={`w-full sm:w-[175px] disabled:opacity-10 cursor-not-allowed`}
+            onClick={() => navigate("/get-measured/manual/measurement")}
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
