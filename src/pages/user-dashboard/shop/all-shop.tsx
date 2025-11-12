86022ec52bf982d8be88d6ed4a30602026ca417e
Romeo <romeomeshack34@gmail.com>
2025-11-12 11:16:57 +0100

    Fixed grid layout

--- src/pages/user-dashboard/shop/all-shop.tsx
+++ src/pages/user-dashboard/shop/all-shop.tsx
@@ -78,7 +78,7 @@ export function AllShop() {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-4 p-8">
       {products.map((product) => (
         <Card
           key={product.id}
