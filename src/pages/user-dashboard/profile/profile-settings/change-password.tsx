57790605b5423d2fd29e75c20d9cecbad00dadfa
Romeo <romeomeshack34@gmail.com>
2025-11-13 11:10:15 +0100

    Modified

--- src/pages/user-dashboard/profile/profile-settings/change-password.tsx
+++ src/pages/user-dashboard/profile/profile-settings/change-password.tsx
@@ -32,6 +32,7 @@ export default function ChangePassword() {
   } = useForm<ChangePassword>();
   const updatePassword = useUpdatePassword();
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const onSubmit: SubmitHandler<ChangePassword> = (data: any) => {
     updatePassword.mutate({
       currentPassword: String(data.currentPassword),
