0d36e88db18a7dad0bb904979cfb47ed08faf7e6
Romeo <romeomeshack34@gmail.com>
2025-11-19 20:05:09 +0100

    Added order status dropdown

--- src/pages/admin-dashboard/orders-management/home.tsx
+++ src/pages/admin-dashboard/orders-management/home.tsx
@@ -1,4 +1,4 @@
-import { CaretRightIcon } from "@phosphor-icons/react";
+import { CaretDownIcon, CaretRightIcon } from "@phosphor-icons/react";
 import TopBar from "../admin-components/top-bar/top-bar";
 import AdminDashboardLayout from "../../../layouts/admin-dashboard/dashboard-layout";
 import Overview from "../admin-components/top-overview-items";
@@ -9,15 +9,111 @@ import Requests from "./request";
 import { useAdminDashboardMetrics } from "../../../hooks/admin-dashboard.hooks";
 import { convertMetricsToOverviewCards } from "../../../utils/admin-dashboard-utils";
 import Spinner from "../../../shared-components/spinner";
+import Button from "../../../shared-components/button";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../../ui/dropdown-menu";
+import { DropdownMenuContent } from "@radix-ui/react-dropdown-menu";
+import { TableButton } from "../../ui/button";
+import { useState } from "react";
+import { useOrderStatusContext, type OrderStatus } from "./order-table-context";
+import { useUpdateBulkOrderStatus } from "../../../hooks/admin-orders.hooks";
+import showToast from "../../../utils/notification";
+import { notificationStyles } from "../../../style/custom";
 
 /* ------------------------------------------------------------------------------------ */
 
+const tableOrderStatus = [
+  { label: "pending", value: "pending", style: "bg-[#FEF3C7] text-[#D97706]" },
+  {
+    label: "approved",
+    value: "approved",
+    style: "bg-[#DCFCE7] text-[#16A34A]",
+  },
+   {
+    label: "packaging",
+    value: "packaging",
+    style: "bg-[#E0F2FE] text-[#0EA5E9]",
+  },
+  { label: "sewing", value: "sewing", style: "bg-[#F0FAFF] text-[#0EA5E9]" },
+  {
+    label: "out-for-delivery",
+    value: "out for delivery",
+    style: "bg-[#EFF4FF] text-[#2563EB]",
+  },
+  {
+    label: "delivered",
+    value: "delivered",
+    style: "bg-[#ECFDF8] text-[#059669]",
+  },
+  {
+    label: "complete",
+    value: "complete",
+    style: "bg-[#F1F5F9] text-[#475569]",
+  },
+  {
+    label: "canceled",
+    value: "canceled",
+    style: "bg-[#FEE2E2] text-[#991B1B]",
+  },
+  {
+    label: "rejected",
+    value: "rejected",
+    style: "bg-[#FEE2E2] text-[#f10606]",
+  },
+];
+
 export function AdminDashboardOrders() {
+  const [orderStatus, setOrderStatus] = useState<null | string>(null);
+  const { selectedTableRow, setSelectedTableRow } = useOrderStatusContext();
+  const { bulkUpdate, isUpdating } = useUpdateBulkOrderStatus();
+
   const {
     data: metrics,
     isLoading: metricsLoading,
     error: metricsError,
   } = useAdminDashboardMetrics();
+
+  const handleBulkUpdateOnChange = async (orders: OrderStatus[]) => {
+    if (!orderStatus) {
+      showToast.warning("Please select order status", {
+        style: notificationStyles.alertWarning,
+        icon: null,
+      });
+      return;
+    }
+
+    if (orders.length === 0) return;
+
+    const orderToUpdate = orders?.map((order) => ({
+      id: order.orderId,
+      status: (orderStatus as string).toLowerCase(),
+    }));
+
+    const result = await bulkUpdate(orderToUpdate);
+
+    if (result.errors.length > 0) {
+      console.error("Failed orders:", result.errors);
+      // Could show a modal with error details
+    }
+
+    // Clear selected table rows and reset status dropdown
+    if (result.successful > 0) {
+      setSelectedTableRow({
+        orders: [],
+        request: [],
+        shouldClearSelection: true,
+      });
+      setOrderStatus(null);
+    }
+  };
+
+  // Get style of selected order status to show after dropdown selection.
+  const selectedOrderStatusStyle = tableOrderStatus.find(
+    (status) => status.value === orderStatus
+  );
 
   const title = (
     <div className="font-lora text-[#1C1C1C]">Order Management</div>
@@ -49,13 +145,16 @@ export function AdminDashboardOrders() {
         <div className="w-full  flex flex-col gap-4 p-4 mt-20 overflow-y-scroll px-10">
           {metricsLoading ? (
             <div className="flex justify-center items-center h-32">
-              <Spinner size="lg" speed="fast" isLoading={metricsLoading} arcColor="#9A6C50" />
+              <Spinner
+                size="lg"
+                speed="fast"
+                isLoading={metricsLoading}
+                arcColor="#9A6C50"
+              />
             </div>
           ) : metricsError ? (
             <div className="bg-red-50 border border-red-200 rounded-md p-4">
-              <p className="text-red-600">
-                Unable to load dashboard metrics.
-              </p>
+              <p className="text-red-600">Unable to load dashboard metrics.</p>
             </div>
           ) : metrics ? (
             <Overview
@@ -84,35 +183,62 @@ export function AdminDashboardOrders() {
               </TabsList>
 
               <div className="flex items-center gap-2">
-                {/*<DropdownMenu>
-                  <DropdownMenuTrigger asChild>
+                <DropdownMenu>
+                  <DropdownMenuTrigger
+                    asChild
+                    onClick={(e) => e.preventDefault()}
+                    disabled={selectedTableRow.orders?.length === 0}
+                  >
                     <TableButton
                       variant="outline"
                       size="sm"
-                      className="w-52 h-11 border border-[#D0D5DD] text-[#676767] text-sm bg-white "
+                      className="w-52 h-11 border border-[#D0D5DD] text-[#676767] text-sm bg-white flex items-center justify-between"
                     >
-                      Change order status
+                      <span
+                        className={`capitalize ${
+                          selectedOrderStatusStyle &&
+                          selectedOrderStatusStyle.style
+                        } text-xs py-1 w-auto rounded-full px-2`}
+                      >
+                        {!orderStatus ? ` Change order status` : orderStatus}
+                      </span>
                       <CaretDownIcon className="text-[#676767]" size={20} />
                     </TableButton>
                   </DropdownMenuTrigger>
 
-                  <DropdownMenuContent className="w-52 h-full flex flex-col items-start gap-3 p-3">
-                    {orderStatuses.map((status) => (
+                  <DropdownMenuContent className="w-52 h-full flex flex-col items-start gap-3 p-3 z-20 bg-[#FFFFFF] border border-gray-100 rounded-md mt-2">
+                    {tableOrderStatus.map((status) => (
                       <DropdownMenuItem
-                        className={`${status.bgColor} ${status.textColor} px-2 cursor-pointer text-xs py-1   w-auto  block  rounded-full`}
-                        key={status.status}
+                        className={`${status.style} px-2 cursor-pointer text-xs py-1 w-auto  block  rounded-full`}
+                        key={status.label}
+                        onClick={() => setOrderStatus(status.value)}
                       >
-                        {status.status}
+                        {status.value}
                       </DropdownMenuItem>
                     ))}
                   </DropdownMenuContent>
-                </DropdownMenu>*/}
+                </DropdownMenu>
 
-                {/*<Button
-                  text="Save"
+                <Button
                   variant="solid"
-                  className=" text-white w-24 h-11 shadow-sm"
-                />*/}
+                  disabled={selectedTableRow.orders?.length === 0}
+                  className="text-white w-32 h-11 shadow-sm disabled:opacity-50"
+                  onClick={() =>
+                    handleBulkUpdateOnChange(
+                      selectedTableRow.orders ? selectedTableRow.orders : []
+                    )
+                  }
+                >
+                  <div className="flex items-center justify-center gap-2">
+                    <span>Save</span>
+                    <Spinner
+                      isLoading={isUpdating}
+                      speed="fast"
+                      size="sm"
+                      arcColor="#ffff"
+                    />
+                  </div>
+                </Button>
               </div>
             </section>
 
@@ -122,7 +248,7 @@ export function AdminDashboardOrders() {
             >
               <Orders />
             </TabsContent>
-            
+
             <TabsContent value="past-performance">
               <Requests />
             </TabsContent>
