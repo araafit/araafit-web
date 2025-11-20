255f508c1160935ae1e825b9844d98f8a24f4ffc
Romeo <romeomeshack34@gmail.com>
2025-11-20 13:58:26 +0100

    Scrollable drawer for overview page

--- src/pages/admin-dashboard/admin-components/overViewTable/overview-data-table.tsx
+++ src/pages/admin-dashboard/admin-components/overViewTable/overview-data-table.tsx
@@ -24,6 +24,8 @@ import { ArrowLeftIcon } from "@phosphor-icons/react";
 import { orderStatuses } from "../../_data/_overview";
 import { useAdminOrder } from "../../../../hooks/admin-orders.hooks";
 import type { AdminOrder } from "../../../../services/admin-orders.service";
+import InfoSection from "../orderTable/info-section";
+import { useMemo } from "react";
 /* ------------------------------------------------------------------------------------------------ */
 
 export function OverviewTable() {
@@ -58,14 +60,105 @@ export function OverviewTable() {
   }
 
   const ViewMoreDrawer = ({ orderId }: { orderId: string }) => {
-    const { isLoading, isSuccess, data, refetch } =
-      useAdminOrder(orderId);
+    const { isLoading, isSuccess, data, refetch } = useAdminOrder(orderId);
 
     const orderData = data ? data : ({} as AdminOrder);
 
     const customerName = orderData.user
       ? `${orderData.user.firstName} ${orderData.user.lastName}`
       : "...";
+
+    const status = orderData.status;
+    const statusStyle = orderStatuses.find(
+      (s) => s.status?.toLowerCase() === status?.toLowerCase()
+    );
+
+    console.log(isLoading, isSuccess, data);
+
+    const orderInformation = useMemo(
+      () => (
+        <InfoSection
+          title="Order information"
+          items={[
+            {
+              label: "Order date & Time",
+              value: orderData.createdAt
+                ? orderData.createdAt.toLocaleLowerCase()
+                : "...",
+            },
+            {
+              label: "Dress",
+              value: orderData.dress ? orderData.dress : "...",
+            },
+            { label: "Dress Size", value: "..." },
+            { label: "Dress Color", value: "..." },
+            { label: "Quantity", value: "..." },
+            {
+              label: "Amount",
+              value: orderData.totalAmount ? orderData.totalAmount : "...",
+            },
+            { label: "Payment Method", value: "Card" },
+          ]}
+        />
+      ),
+      [orderData]
+    );
+
+    const deliveryInformation = useMemo(
+      () => (
+        <InfoSection
+          title="Delivery Information"
+          items={[
+            {
+              label: "Delivery date & Time",
+              value: orderData.deliveryDate
+                ? orderData.deliveryDate.toLocaleLowerCase()
+                : "...",
+            },
+            {
+              label: "customer Name",
+              value: customerName,
+            },
+            {
+              label: "Email",
+              value: orderData.user ? orderData.user.email : "...",
+            },
+            {
+              label: "Address",
+              value: orderData.user ? orderData.user.deliveryAddress : "...",
+            },
+            {
+              label: "City/Town",
+              value: orderData.user ? orderData.user.city : "...",
+            },
+            {
+              label: "Phone",
+              value: orderData.user ? orderData.user.phoneNumber : "...",
+            },
+          ]}
+        />
+      ),
+      [orderData]
+    );
+
+    const riderInformation = useMemo(
+      () => (
+        <InfoSection
+          title="Order information"
+          items={[
+            {
+              label: "Rider's name",
+              value: orderData.rider ? orderData.rider.name : "...",
+            },
+            {
+              label: "Rider's phone Number",
+              value: orderData.rider ? orderData.rider.phone : "...",
+            },
+          ]}
+        />
+      ),
+      [orderData]
+    );
 
     return (
       <Drawer>
@@ -78,13 +171,13 @@ export function OverviewTable() {
         </DrawerTrigger>
 
         <DrawerContent
-          className="w-[38.313rem] p-4"
+          className="h-screen w-[38.313rem] p-4 overflow-y-scroll overflow-x-hidden"
           aria-describedby="order-management-detail"
         >
           <DrawerHeader>
-            {isSuccess && !data ? (
+            {!isLoading && !data ? (
               <div className="flex items-center justify-center">
-                <span className="text-neutral-300">...</span>
+                <span className="text-primary-500">...</span>
               </div>
             ) : (
               <div className="flex items-center gap-4">
@@ -98,22 +191,15 @@ export function OverviewTable() {
                   Order ID: {data?.id.substring(0, 6)} ...
                 </DrawerTitle>
 
-                {(() => {
-                  const status = orderData.status;
-                  const style = orderStatuses.find((s) => s.status === status);
-
-                  return (
-                    <div
-                      className={`px-2 py-1 text-xs rounded-full w-fit ${
-                        style
-                          ? `${style.bgColor} ${style.textColor}`
-                          : "bg-gray-100 text-gray-600"
-                      }`}
-                    >
-                      {status}
-                    </div>
-                  );
-                })()}
+                <div
+                  className={`px-2 py-1 text-xs rounded-full w-fit ${
+                    statusStyle
+                      ? `${statusStyle.bgColor} ${statusStyle.textColor}`
+                      : "bg-gray-100 text-gray-600"
+                  }`}
+                >
+                  {status}
+                </div>
               </div>
             )}
           </DrawerHeader>
@@ -129,158 +215,22 @@ export function OverviewTable() {
             </div>
           )}
 
-          {isSuccess && !data && (
-            <div className="size-full">
-              <p className="text-neutral-300 font-medium">No order detail</p>
+          {!isLoading && !data && (
+            <div className="size-full flex items-center justify-center">
+              <p className="font-light text-primary-500">No order detail</p>
             </div>
           )}
 
           {isSuccess && data && (
-            <div className="w-full flex flex-col items-center gap-4 divide-y divide-neutral-200 overflow-y-scroll pb-2">
+            <div className="w-full flex flex-col items-start gap-4 divide-y divide-neutral-200 overflow-y-scroll pb-2">
               {/* Order information */}
-              <div className="w-full py-[12px] px-4 flex flex-col justify-center gap-4">
-                <h3 className="w-full text-neutral-700 font-inter font-medium">
-                  Order Information
-                </h3>
-
-                <div className="w-full flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    Order Date & Time:
-                  </span>
-                  <span className="text-neutral-600 text-[14px] font-medium">
-                    {orderData.createdAt}
-                  </span>
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    Dress:
-                  </span>
-                  {/* <span className="text-neutral-600 text-[14px] font-medium">{orderData.}</span> */}
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    Dress Size:
-                  </span>
-                  <span className="text-neutral-600 text-[14px] font-medium">
-                    Yellow & Black
-                  </span>
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    Quantity:
-                  </span>
-                  <span className="text-neutral-600 text-[14px] font-medium">
-                    2
-                  </span>
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    Amount:
-                  </span>
-                  <span className="text-neutral-600 text-[14px] font-medium">
-                    ₦200,000.00
-                  </span>
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    Payment Method:
-                  </span>
-                  <span className="text-neutral-600 text-[14px] font-medium">
-                    Card
-                  </span>
-                </div>
-              </div>
+              {orderInformation}
 
               {/* Delivery information */}
-              <div className="w-full py-[12px] px-4 flex flex-col justify-center gap-4">
-                <h3 className="w-full text-neutral-700 font-inter font-medium">
-                  Delivery Information
-                </h3>
-
-                <div className="w-full flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    Delivery Date & Time:
-                  </span>
-                  <span className="text-neutral-600 text-[14px] font-medium">
-                    {orderData.deliveryDate ? orderData.deliveryDate : "..."}
-                  </span>
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    Customer Name
-                  </span>
-                  <span className="text-neutral-600 text-[14px] font-medium">
-                    {customerName}
-                  </span>
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    Email:
-                  </span>
-                  <span className="text-neutral-600 text-[14px] font-medium">
-                    {orderData.user ? orderData.user.email : "..."}
-                  </span>
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    Address:
-                  </span>
-                  <span className="text-neutral-600 text-[14px] font-medium">
-                    {orderData.user ? orderData.user.deliveryAddress : "..."}
-                  </span>
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    City:
-                  </span>
-                  <span className="text-neutral-600 text-[14px] font-medium">
-                    {orderData.user ? orderData.user.email : "..."}
-                  </span>
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    Phone:
-                  </span>
-                  <span className="text-neutral-600 text-[14px] font-medium">
-                    {orderData.user ? orderData.user.phoneNumber : "..."}
-                  </span>
-                </div>
-              </div>
+              {deliveryInformation}
 
               {/* Rider information */}
-              <div className="w-full py-[12px] px-4 flex flex-col justify-center gap-4">
-                <h3 className="w-full text-neutral-700 font-inter font-medium">
-                  Rider Information
-                </h3>
-
-                <div className="w-full flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    Rider’s Name:
-                  </span>
-                  <span className="text-neutral-600 text-[14px] font-medium">
-                    {orderData.rider ? orderData.rider.name : "..."}
-                  </span>
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <span className="text-neutral-400 text-[14px] font-light">
-                    Rider’s Phone Number:
-                  </span>
-                  <span className="text-neutral-600 text-[14px] font-medium">
-                    {orderData.rider ? orderData.rider.phone : "..."}
-                  </span>
-                </div>
-              </div>
+              {riderInformation}
             </div>
           )}
         </DrawerContent>
